7bbdbd8d352701f33a539d73279194c7105c7d28
Ewan G <ewang@ITs-MacBook-Pro.local>
2023-03-22 14:44:15 +0000

    third commit

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -7,3 +7,11 @@ def add(num_1, num_2):
 def subtract(num_1, num_2):
     return num_1 - num_2
 
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string( test_string ):
+    return len(test_string)
